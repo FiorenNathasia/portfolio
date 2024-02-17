e71e86443f5ef7702e84018fa66348980243fabc
FiorenNathasia <fiorennathasia@gmail.com>
2024-02-17 00:36:14 +0000

    Finish styling carousel mobile and desktop.

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { motion } from "framer-motion";
 import "./Projects.scss";
 import thrivevibe from "../../assets/video/ThriveVibeDemo.mp4";
 import loom from "../../assets/video/ThriveVibeLoom.mp4";
@@ -15,7 +16,7 @@ const Projects = () => {
       title: "ThriveVibe",
       video: thrivevibe,
       description:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
       frontend: "HTML5, CSS, SASS, JavaScript, React",
       backend: "NodeJS, Express.JS, Knex, MySql",
       apis: "none",
@@ -24,7 +25,7 @@ const Projects = () => {
       title: "Loom",
       video: loom,
       description:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
       frontend: "HTML5, CSS, SASS, JavaScript, React",
       backend: "NodeJS, Express.JS, Knex, MySql",
       apis: "none",
@@ -33,7 +34,7 @@ const Projects = () => {
       title: "Demo",
       video: demo,
       description:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
       frontend: "HTML5, CSS, SASS, JavaScript, React",
       backend: "NodeJS, Express.JS, Knex, MySql",
       apis: "none",
@@ -41,42 +42,51 @@ const Projects = () => {
   ];
 
   const length = projects.length;
+
   const nextProject = () => {
-    setCurrent(current === length - 1 ? 0 : current + 1);
+    setCurrent((current + 1) % length);
   };
 
   const prevProject = () => {
-    setCurrent(current === 0 ? length - 1 : current - 1);
+    setCurrent((current - 1 + length) % length);
   };
-  if (!Array.isArray(projects) || projects.length <= 0) {
-    return null;
-  }
+
   return (
-    <>
-      <div className="projects">
-        {projects.map((project, index) => {
-          return (
-            <div
-              className={index === current ? "project active" : "slide"}
-              key={index}
-            >
-              {index === current && (
-                <Projectscard project={project} index={index} />
-              )}
-            </div>
-          );
-        })}{" "}
+    <div className="projects">
+      <h1 className="projects__title">My Projects</h1>
+      <div className="projects__container">
+        {projects.map((project, index) => (
+          <motion.div
+            key={index}
+            className="project"
+            initial={{ opacity: 0, x: "-100%" }}
+            animate={{
+              opacity: index === current ? 1 : 0,
+              x: index === current ? "0%" : "-100%",
+            }}
+            transition={{ duration: 0.5 }}
+          >
+            {index === current && <Projectscard project={project} />}
+          </motion.div>
+        ))}
         <div className="projects__buttons">
-          {" "}
-          <button className="projects__arrow-left" onClick={prevProject}>
+          <motion.button
+            className="projects__arrow-left"
+            onClick={prevProject}
+            whileHover={{ scale: 1.1 }}
+          >
             <img src={leftarrow} />
-          </button>
-          <button className="projects__arrow-right" onClick={nextProject}>
+          </motion.button>
+          <motion.button
+            className="projects__arrow-right"
+            onClick={nextProject}
+            whileHover={{ scale: 1.1 }}
+          >
             <img src={rightarrow} />
-          </button>
+          </motion.button>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
