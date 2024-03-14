df4c40f30c5243df9de142ed3c6014d8e8efa08e
FiorenNathasia <fiorennathasia@gmail.com>
2024-03-14 21:28:52 +0000

    Fix about page.

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -23,51 +23,53 @@ const About = () => {
   };
 
   return (
-    <div className="about__container">
-      <motion.div
-        className="about__text"
-        initial="initial"
-        animate={inView ? "animate" : "initial"}
-        ref={ref}
-        variants={variants}
-      >
-        {inView && (
-          <>
-            <h1 className="about__title">About Me</h1>
-            <p className="about__paragraph-one">
-              Transitioning from a lifelong passion for visual arts to software
-              engineering was a choice caused by a desire for a greater
-              challenge. The constantly changing industry of technology
-              interested me, pushing me to pursue it. Joining Brainstation's
-              software engineering bootcamp marked the beginning of this new
-              chapter.
-            </p>
-            <p className="about__paragraph-two">
-              From my time as a textile designer, I bring skills like a keen eye
-              for aesthetics, a focus on user-centered design, and a natural
-              understanding of visual appeal. Additionally, the problem-solving
-              skills from my time as a designer fit perfectly with the many
-              challenges of software engineering, where innovative solutions are
-              continuously needed to bring new ideas to life. This comprehensive
-              perspective allows me to anticipate and address potential issues,
-              highlighting my versatility and creativity as a full-stack
-              developer.
-            </p>
-            <p className="about__paragraph-three">
-              With a solid understanding of coding basics, I am driven by a
-              strong desire for knowledge and excellence, laying a strong
-              foundation for ongoing growth in the field of software
-              engineering. I aim to continuously improve and learn more, fueled
-              by the many opportunities and excitement in the tech industry. I
-              am prepared to tackle any new challenge with determination,
-              striving to surpass previous achievements and explore new
-              possibilities along the way!
-            </p>
-          </>
-        )}
-      </motion.div>
-      <Cube />
-      <div className="about__techstack">
+    <div className="about">
+      <div className="about__container">
+        <motion.div
+          className="about__text"
+          initial="initial"
+          animate={inView ? "animate" : "initial"}
+          ref={ref}
+          variants={variants}
+        >
+          {inView && (
+            <>
+              <h1 className="about__title">About Me</h1>
+              <p className="about__paragraph-one">
+                Transitioning from a lifelong passion for visual arts to
+                software engineering was a choice caused by a desire for a
+                greater challenge. The constantly changing industry of
+                technology interested me, pushing me to pursue it. Joining
+                Brainstation's software engineering bootcamp marked the
+                beginning of this new chapter.
+              </p>
+              <p className="about__paragraph-two">
+                From my time as a textile designer, I bring skills like a keen
+                eye for aesthetics, a focus on user-centered design, and a
+                natural understanding of visual appeal. Additionally, the
+                problem-solving skills from my time as a designer fit perfectly
+                with the many challenges of software engineering, where
+                innovative solutions are continuously needed to bring new ideas
+                to life. This comprehensive perspective allows me to anticipate
+                and address potential issues, highlighting my versatility and
+                creativity as a full-stack developer.
+              </p>
+              <p className="about__paragraph-three">
+                With a solid understanding of coding basics, I am driven by a
+                strong desire for knowledge and excellence, laying a strong
+                foundation for ongoing growth in the field of software
+                engineering. I aim to continuously improve and learn more,
+                fueled by the many opportunities and excitement in the tech
+                industry. I am prepared to tackle any new challenge with
+                determination, striving to surpass previous achievements and
+                explore new possibilities along the way!
+              </p>
+            </>
+          )}
+        </motion.div>
+
+        <Cube />
+
         <Techstack />
       </div>
     </div>
--- src/components/hero/Hero2.jsx
+++ src/components/hero/Hero2.jsx
@@ -11,54 +11,60 @@ import bluecorner from "../../assets/decorations/bluecorner.svg";
 import headshot from "../../assets/headshot.png";
 import scroll from "../../assets/decorations/scroll.png";
 
-// const textVariants = {
-//   initial: {
-//     x: 500,
-//     opacity: 0,
-//   },
+const textVariants = {
+  initial: {
+    x: 500,
+    opacity: 0,
+  },
 
-//   animate: {
-//     x: 0,
-//     opacity: 1,
-//     transition: {
-//       duration: 1,
-//       staggerChildren: 0.1,
-//     },
-//   },
-//   scrollButton: {
-//     opacity: 0,
-//     y: 10,
-//     transition: {
-//       duration: 2,
-//       repeat: Infinity,
-//     },
-//   },
-// };
+  animate: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      duration: 1,
+      staggerChildren: 0.1,
+    },
+  },
+  scrollButton: {
+    opacity: 0,
+    y: 10,
+    transition: {
+      duration: 2,
+      repeat: Infinity,
+    },
+  },
+};
 
 const Hero2 = () => {
   return (
-    <div className="hero">
+    <div className="hero2">
       <Sidebar />
+
       <div className="hero2__wrapper">
         <img className="hero2__yellowarrow" src={yellowarrow}></img>
-        <div className="hero2__textcontainer">
-          <h2 className="hero2__h2">Hi I'm Fioren Nathasia!</h2>
-          <h1 className="hero2__h1">
+        <motion.div
+          className="hero2__textcontainer"
+          variants={textVariants}
+          initial="initial"
+          animate="animate"
+        >
+          <motion.h2 className="hero2__h2">Hi I'm Fioren Nathasia!</motion.h2>
+          <motion.h1 className="hero2__h1">
             a Full-stack Software Engineer and Web Developer
-          </h1>
-          <div className="hero2__slogan">
-            <img
+          </motion.h1>
+          <motion.div className="hero2__slogan">
+            <motion.img
               className="hero2__scroll"
               src={scroll}
-              //   variants={textVariants}
-              //   animate="scrollButton"
-            ></img>
-            <h3 className="hero2__h3">
+              variants={textVariants}
+              animate="scrollButton"
+            ></motion.img>
+            <motion.h3 className="hero2__h3">
               I am a recent graduate of Brainstation’s Software Engineering
               Bootcamp in Dec 2023 aiming to pursue a career in coding!
-            </h3>
-          </div>
-        </div>
+            </motion.h3>
+          </motion.div>
+        </motion.div>
         <div className="hero2__imagecontainer">
           <img className="hero2__yellowx" src={yellowx}></img>
           <img className="hero2__headshot" src={headshot}></img>
